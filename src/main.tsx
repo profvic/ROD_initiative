81755f6e29ecd180f11961baa1ef98bcff6c0e08
Chidiadi Anyanwu <chidiadi01@gmail.com>
2025-02-16 18:11:22 +0100

    PROBLEMS INTRODUCED: - The width of the page is now smaller than the screen.
    Changes made:
    - Deleted App.tsx. I think the HomePage component is enough.

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.tsx";
 import DonatePage from "./components/pages/donatePage/DonatePage.tsx";
+import HomePage from "./components/pages/homePage/HomePage.tsx";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<HomePage/>} />
       <Route path="/donate" element={<DonatePage />} />
     </Routes>
   </BrowserRouter>
